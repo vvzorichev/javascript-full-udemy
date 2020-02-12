079fd6da0f3846662e3891152361c7da0acd5527
Vitaly <vitalyzorich@gmail.com>
2020-02-12 23:17:15 +0300

    proj_4 3

--- proj_4/js/script.js
+++ proj_4/js/script.js
@@ -34,4 +34,52 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    let deadline = '2020-02-14';
+
+    function getTimeRemaining(endtime) {
+        let time = Date.parse(endtime) - Date.parse(new Date),
+            seconds = Math.floor((time / 1000) % 60),
+            minutes = Math.floor((time / 1000 / 60) % 60),
+            hours = Math.floor((time / 1000 / 60 / 60));
+        
+        return {
+            'total' : time,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        }
+    };
+
+    function setClock(id, endtime) {
+        let timer = document.querySelector(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let timeToDeadline = getTimeRemaining(endtime);
+
+            function addZero(num){
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            hours.textContent = addZero(timeToDeadline.hours);
+            minutes.textContent = addZero(timeToDeadline.minutes);
+            seconds.textContent = addZero(timeToDeadline.seconds);
+
+            if (timeToDeadline.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            };
+        };
+
+    };
+
+    setClock('#timer', deadline);
 });
