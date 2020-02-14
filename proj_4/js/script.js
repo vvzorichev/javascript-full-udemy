049e05e378991d020d43fc8f1fd6bb9070b673c6
Vitaly <vitalyzorich@gmail.com>
2020-02-14 23:10:34 +0300

    proj_4 5

--- proj_4/js/script.js
+++ proj_4/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
 
     'use strict';
 
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    info.addEventListener('click', function() {
+    info.addEventListener('click', () => {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
             for (let i = 0; i < tab.length; i++) {
@@ -94,17 +94,17 @@ window.addEventListener('DOMContentLoaded', function() {
         close = document.querySelector('.popup-close'),
         moreBtns = [more, ...document.querySelectorAll('.description-btn')];
 
-    moreBtns.forEach(function(item) {
-        item.addEventListener('click', function() {
+    moreBtns.forEach( (item) => {
+        item.addEventListener('click', () => {
             overlay.style.display = 'block';
             this.classList.add('more-splash');
             document.body.style.overflow = 'hidden';
         });
     });
 
-    close.addEventListener('click', function() {
+    close.addEventListener('click', () => {
         overlay.style.display = 'none';
-        moreBtns.forEach(function(item) {
+        moreBtns.forEach( (item) => {
             item.classList.remove('more-splash');
         });
         document.body.style.overflow = '';
