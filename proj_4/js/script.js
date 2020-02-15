00ff5d71a94cf2f5d2b1a23245a1b8bafbafe005
Vitaly <vitalyzorich@gmail.com>
2020-02-15 19:04:07 +0300

    proj_4 5

--- proj_4/js/script.js
+++ proj_4/js/script.js
@@ -109,4 +109,53 @@ window.addEventListener('DOMContentLoaded', () => {
         });
         document.body.style.overflow = '';
     });
+
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        succses: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let forms = document.querySelectorAll('form'),
+        input = document.querySelectorAll('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    forms.forEach((item) => {
+        item.addEventListener('submit', (event) => {
+            event.preventDefault();
+            item.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(item);
+            console.log(formData);
+            let obj = {};
+            formData.forEach((value, key) => {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState < 4) {
+                    statusMessage.textContent = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.textContent = message.succses;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+
+            input.forEach((item) => {
+                item.value = '';
+            });
+        });
+    });
 });
