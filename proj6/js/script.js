c6f41b49be613f7292b2bfe8f077e69ad671d7c8
Vitaly <vitalyzorich@gmail.com>
2020-02-19 18:16:43 +0300

    proj 4 and proj 6

---
+++ proj6/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('.main_btna, .main_btn, a[href="#sheldure"]').on('click', () => {
+        $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+    });
+
+    $('.close').on('click', () => {
+        $('.overlay').fadeOut('slow');
+        $('.modal').slideUp('slow');
+        
+    });
+});
